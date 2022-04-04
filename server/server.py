1258ecd9967e2d735411235e0a290d1871de214a
sleipnir-lnx <52344836+sleipnir029@users.noreply.github.com>
2022-04-04 12:46:16 +0600

    get_location_names

--- server/server.py
+++ server/server.py
@@ -1,9 +1,14 @@
+from urllib import response
 from flask import Flask, request,  jsonify
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "hi"
+@app.route('/get_location_name')
+def get_location_names():
+    response = jsonify({
+        'location': util.get_location_names()
+    })
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 
 
