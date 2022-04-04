767665214a429a2541b568eb9ed5c0d27ab2017e
sleipnir-lnx <52344836+sleipnir029@users.noreply.github.com>
2022-04-04 13:21:49 +0600

    make model global

--- server/util.py
+++ server/util.py
@@ -15,7 +15,7 @@ def get_estimated_price(location, sqft, bhk, bath):
     x[0], x[1], x[2] = sqft, bath, bhk
     if loc_index >= 0:
         x[loc_index] = 1
-    return __model.predict([x])
+    return round(__model.predict([x])[0], 2)
 
 def get_location_names():
     return __locations
@@ -24,6 +24,7 @@ def load_saved_artifacts():
     print("Loading saved artifacts...start")
     global __data_columns
     global __locations
+    global __model
     
     with open('./artifacts/columns.json', 'r') as f:
         __data_columns = json.load(f)['data_columns']
@@ -37,3 +38,7 @@ def load_saved_artifacts():
 if __name__ == "__main__":
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))  # other locations
+    print(get_estimated_price('Ejipura', 1000, 2, 2))   # other locations
